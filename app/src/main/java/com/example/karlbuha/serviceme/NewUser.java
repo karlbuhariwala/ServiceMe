2eded83eb49dcfa24210ee58b04b903470d246fc
karlbuhariwala <karlbuhariwala@gmail.com>
2015-08-02 17:35:54 -0700

    FormatingNewUserPage

--- app/src/main/java/com/example/karlbuha/serviceme/NewUser.java
+++ app/src/main/java/com/example/karlbuha/serviceme/NewUser.java
@@ -2,9 +2,17 @@ package com.example.karlbuha.serviceme;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.lang.String;
 
 
 public class NewUser extends Activity {
@@ -13,6 +21,23 @@ public class NewUser extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+
+        Button sendVerificationCodeButton = (Button) findViewById(R.id.sendVerificationCodeButton);
+        sendVerificationCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText phoneNumberEditText = (EditText) findViewById(R.id.phoneNumberEditText);
+                String phoneNumber = phoneNumberEditText.getText().toString();
+                TextView phoneNumberTextView = (TextView) findViewById(R.id.phoneNumberTextView);
+                if(!PhoneNumberUtils.isGlobalPhoneNumber(phoneNumber)) {
+                    phoneNumberTextView.setTextColor(getResources().getColor(R.color.red));
+                }
+                else
+                {
+                    phoneNumberTextView.setTextColor(getResources().getColor(R.color.white));
+                }
+            }
+        });
     }
 
     @Override
